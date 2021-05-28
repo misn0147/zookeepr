e2007a43b9466be568649f42af4bf0c08aff8038
M Natoli <natoli.missy@gmail.com>
2021-05-27 17:43:47 -0700

    fixed indent

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
